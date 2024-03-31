8884b2e3d5403a2e63674fd94b2a22d1f4471c26
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2024-03-31 13:42:22 +0000

    update

--- src/components/MyServices/Services.jsx
+++ src/components/MyServices/Services.jsx
@@ -5,6 +5,9 @@ import services from "/src/ServiceData.json";
 const Services = () => {
   return (
     <div>
+      <div>
+        <h1 className="service-header text-center pt-8">Services</h1>
+      </div>
       {services.map((service) => (
         <Service service={service} key={service.id} />
       ))}
